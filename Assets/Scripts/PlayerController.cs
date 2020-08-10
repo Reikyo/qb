0771b674cdcb6cd91a015e36c59bf2f607435f4b
Darren Temple <darren.temple.101@gmail.com>
2020-08-10 21:01:05 +0100

    - Started off player controller script using forces - Constrained player rotation about the x and z axes in order to stop the capsule from falling over

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float force = 1000.0f;
+    private Rigidbody rbPlayer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rbPlayer = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float inputHorz = Input.GetAxis("Horizontal");
+        float inputVert = Input.GetAxis("Vertical");
+
+        rbPlayer.AddForce(inputHorz * force * Time.deltaTime * Vector3.right);
+        rbPlayer.AddForce(inputVert * force * Time.deltaTime * Vector3.forward);
     }
 }
