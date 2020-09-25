46b2a6ca9b815fbc255d67a847119b763054cac6
Darren Temple <darren.temple.101@gmail.com>
2020-09-25 18:09:43 +0100

    - Updated power-up prefab to be a spinning cube with power-up amount text on the sides - Updated projectile prefab to have the same colour as the power-up, and also a motion trail as it flies

---
+++ Assets/Scripts/PowerUpController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpController : MonoBehaviour
+{
+    private float fDegreesPerSecond = 90f;
+    private float fDegreesPerFrame;
+
+    // ------------------------------------------------------------------------------------------------
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fDegreesPerFrame = fDegreesPerSecond * Time.deltaTime;
+    }
+
+    // ------------------------------------------------------------------------------------------------
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, fDegreesPerFrame, 0f, Space.World);
+    }
+
+    // ------------------------------------------------------------------------------------------------
+
+}
